8ad5f40dd8bfe38a5b5c921610c7e7d23c4d17e0
German <magicnova@hotmail.com>
2017-11-30 22:03:34 -0300

    fix key on thread

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
   <h1>Welcome to the forum </h1>
-	<div   v-bind:key="thread.id" v-for="thread in threads">
-		<h2>{{thread.title}}</h2>
+	<div   v-bind:key="thread.slug" v-for="thread in threads">
+		<h2>{{thread.title}}</h2>		
 		<div v-bind:key="postId" v-for="postId in thread.posts">
 			<p>{{users[posts[postId].userId].name}}</p>
 			<p>{{posts[postId].text}}</p>
@@ -13,7 +13,6 @@
 
 <script>
 import sourceData from '@/data'
-console.log(sourceData)
 export default {
 	name: 'HelloWorld',
 	data() {
