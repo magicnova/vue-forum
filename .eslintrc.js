f9ad86c1601babdfcad2e7a34f1a0bd3079f135c
German <magicnova@hotmail.com>
2017-11-30 21:55:33 -0300

    render mockup data

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,10 @@ module.exports = {
 		'generator-star-spacing': 0,
 		// allow debugger during development
 		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-		'no-unused-vars': 1
+		'no-unused-vars': 1,
+		'no-extra-semi': 1,
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		'space-before-function-paren': 0
 	}
 }
